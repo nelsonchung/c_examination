add88abaa80059d42945e3955b9d438b790e19dc
nelson_chung <nelson_chung@askey.com.tw>
2016-12-13 10:48:32 +0800

    排版 for charint.c

--- charint.c
+++ charint.c
@@ -8,14 +8,14 @@
 
 int main(int argc, char* argv[]){
 
-        unsigned char ctmp[8] = "53589793";
-        unsigned int  itmp    = 0x53589793;
+    unsigned char ctmp[8] = "53589793";
+    unsigned int  itmp    = 0x53589793;
 
-        printf("ctmp is %s\n", ctmp);
-        int len = sizeof(ctmp)/sizeof(char);
-        printf("the length of ctmp is %d\n", len);
-        printf("itmp is 0x%x\n", itmp);
-        len = sizeof(itmp);
-        printf("the length of itmp is %d\n", len);
+    printf("ctmp is %s\n", ctmp);
+    int len = sizeof(ctmp)/sizeof(char);
+    printf("the length of ctmp is %d\n", len);
+    printf("itmp is 0x%x\n", itmp);
+    len = sizeof(itmp);
+    printf("the length of itmp is %d\n", len);
 }
 
