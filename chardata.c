539ae537004ecb826de87402ebb5df8548c5772f
nelson_chung <nelson_chung@askey.com.tw>
2016-12-13 10:36:28 +0800

    修正錯誤, %.10s, 要加上.才能夠指定長度.

--- chardata.c
+++ chardata.c
@@ -7,9 +7,10 @@ typedef unsigned char       Uint8;
 int main(int argc, char* argv[])
 {
     Uint8 cvcStartTime[UTC_STRING_LENGTH+1] = "010711000000Z";
+    int cvcStartTime_len = strlen(cvcStartTime);
 
     printf("NelsonDBG: (%s)(%s)(%d) 0x%x\n", __FILE__, __FUNCTION__, __LINE__, cvcStartTime[0]);
-    printf("NelsonDBG: (%s)(%s)(%d) 0x%13s\n", __FILE__, __FUNCTION__, __LINE__, cvcStartTime);
-    printf("NelsonDBG: (%s)(%s)(%d) 0x%*s\n", __FILE__, __FUNCTION__, __LINE__, strlen(cvcStartTime), cvcStartTime);
+    printf("NelsonDBG: (%s)(%s)(%d) len = %d, 0x%s\n", __FILE__, __FUNCTION__, __LINE__, cvcStartTime_len, cvcStartTime);
+    printf("NelsonDBG: (%s)(%s)(%d) 0x%.10s\n", __FILE__, __FUNCTION__, __LINE__, cvcStartTime);
 
 }
