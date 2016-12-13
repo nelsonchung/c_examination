81050f254d5981c9174d13fbf50117ccc8c5b399
nelson_chung <nelson_chung@askey.com.tw>
2016-12-13 10:42:12 +0800

    for chardata.c 更新結果與說明

--- chardata.c
+++ chardata.c
@@ -9,8 +9,8 @@ int main(int argc, char* argv[])
     Uint8 cvcStartTime[UTC_STRING_LENGTH+1] = "010711000000Z";
     int cvcStartTime_len = strlen(cvcStartTime);
 
-    printf("NelsonDBG: (%s)(%s)(%d) 0x%x\n", __FILE__, __FUNCTION__, __LINE__, cvcStartTime[0]);
-    printf("NelsonDBG: (%s)(%s)(%d) len = %d, 0x%s\n", __FILE__, __FUNCTION__, __LINE__, cvcStartTime_len, cvcStartTime);
-    printf("NelsonDBG: (%s)(%s)(%d) 0x%.10s\n", __FILE__, __FUNCTION__, __LINE__, cvcStartTime);
+    printf("0x%x\n", cvcStartTime[0]);
+    printf("len = %d, 0x%s\n", cvcStartTime_len, cvcStartTime);
+    printf("0x%.10s\n", cvcStartTime);
 
 }
