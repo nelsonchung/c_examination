f700c1269d78b8d5e799c8811ff191e5681f4dcb
nelson_chung <nelson_chung@askey.com.tw>
2016-12-13 10:56:34 +0800

    Fix build error. 更新說明文件 for diff_strlen_sizeof.c

--- diff_strlen_sizeof.c
+++ diff_strlen_sizeof.c
@@ -1,3 +1,7 @@
+/*
+* 用意：了解strlen and sizeof的使用方式,以及char, int的長度關係
+*/
+
 #include <stdio.h>
 #include <string.h>
 
@@ -12,10 +16,15 @@ int main(int argc, char* argv[]){
     char str[] = "I am Nelson";
     int five = 5;
 
-    printf("strlen(str) is %d\n", strlen(str));
-    printf("sizeof(str) is %d\n", sizeof(str));
-    printf("sizeof(char) is %d\n", sizeof(char));
-    printf("sizeof(int) is %d\n", sizeof(int));
-    printf("sizeof(five) is %d\n", sizeof(five));
+    int len = strlen(str);
+    printf("strlen(str) is %d\n", len);
+    len = sizeof(str);
+    printf("sizeof(str) is %d\n", len);
+    len = sizeof(char);
+    printf("sizeof(char) is %d\n", len);
+    len = sizeof(int);
+    printf("sizeof(int) is %d\n", len);
+    len = sizeof(five);
+    printf("sizeof(five) is %d\n", len);
 }
 
