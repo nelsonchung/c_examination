282318d298282890518eca93ccb13a2e64164c3e
nelson_chung <nelson_chung@askey.com.tw>
2016-12-13 11:09:42 +0800

    修正說明與輸出結果 for ifdef.c

--- ifdef.c
+++ ifdef.c
@@ -11,45 +11,40 @@
 #define DBG_ONE 1
 
 int main(int argc, char* argv){
+
 #ifdef DEBUG_IFDEF
-    //因為有定義DEBUG_IFDEF，所以會print
-    printf("#ifdef sample code. Define DEBUG_IFDEF\n");
+    printf("(OptionA)#ifdef sample code. Define DEBUG_IFDEF\n");
 #endif
 
 #ifndef DEBUG
-    //因為"沒有"定義DEBUG，所以會print
-    printf("#ifndef sample code. Not define DEBUG\n");
+    printf("(OptionB)#ifndef sample code. Not define DEBUG\n");
 #endif
 
 #if defined(DEBUG_IFDEF) || defined(DEBUG)
-    //DEBUG_IFDEF or DEBUG 其中一個有define，就會printf
-    printf("#if defined() sample code. Define DEBUG_IFDEF, or define DEBUG\n");
+    printf("(OptionC)#if defined() sample code. Define DEBUG_IFDEF, or define DEBUG\n");
 #endif
 
 #if defined(DEBUG_IFDEF) && defined(DEBUG)
-    //DEBUG_IFDEF and DEBUG 兩個都要有define，才會printf
-    printf("#if defined() sample code. Define DEBUG_IFDEF, and define DEBUG\n");
+    printf("(OptionD)#if defined() sample code. Define DEBUG_IFDEF, and define DEBUG\n");
 #endif
     
 #if defined(DEBUG_IFDEF) || defined(DEBUG_ELIF)
-    //DEBUG_IFDEF or DEBUG_ELIF其中一個有define，就會printf
-    printf("#if defined() sample code. Define DEBUG_IFDEF and DEBUG_ELIF\n");
+    printf("(OptionE)#if defined() sample code. Define DEBUG_IFDEF and DEBUG_ELIF\n");
 #endif
 
 #if defined(DEBUG_IFDEF) && defined(DEBUG_ELIF) && defined(DEBUG)
-    //DEBUG_IFDEF, DEBUG_ELIF and DEBUG三個都要define，才會printf
-    printf("#if defined() sample code. Define DEBUG_IFDEF, DEBUG_ELIF and not define DEBUG\n");
+    printf("(OptionF)#if defined() sample code. Define DEBUG_IFDEF, DEBUG_ELIF and not define DEBUG\n");
 #endif
 
 #if DBG_ONE 
-    printf("#if sample code. define DBG_ONE 1\n");
+    printf("(OptionG1)#if sample code. define DBG_ONE 1\n");
 #else
-    printf("#else sample code\n");
+    printf("(OptionG2)#else sample code\n");
 #endif
 
 #if DBG_ZERO
-    printf("#if sample code. define DBG_TWO 2\n");
+    printf("(OptionH1)#if sample code. define DBG_TWO 2\n");
 #else
-    printf("#else sample code.\n");
+    printf("(OptionH2)#else sample code.\n");
 #endif
 }
